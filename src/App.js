7b70ec8bd13db8eb6524d00ce56bcdbba7b62c51
juyoun kim <juyoun.kim@unitedblades.co.jp>
2019-03-25 19:00:04 +0900

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OpenDialog from './containers/openDialog';
+import ButtonAppBar from './containers/menuBar';
+import SimpleCard from './containers/simpleCard';
 
 class App extends Component {
+  showProps = ()=>{
+    console.log("showprops")
+    console.log()
+  }
+
   render() {
     return (
       <div className="App">
+        <ButtonAppBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <SimpleCard/>
+          <OpenDialog/>
           <a
             className="App-link"
             href="https://reactjs.org"
