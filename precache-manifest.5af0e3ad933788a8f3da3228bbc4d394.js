4a0217908d494de7ec72f45cd51937dabcdae9a6
makka <aa019@hotmail.com>
2019-04-02 23:58:37 +0900

    deploy

---
+++ precache-manifest.5af0e3ad933788a8f3da3228bbc4d394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2310ffa25bfb400ad040c2ae323f2103",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/static/media/logo.2310ffa2.svg"
+  },
+  {
+    "revision": "52772a63af646fb2b0b0",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/static/js/runtime~main.4d2af931.js"
+  },
+  {
+    "revision": "d4a0eefe11f494303c9d",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/static/js/main.afb1e065.chunk.js"
+  },
+  {
+    "revision": "f59baac4a8b4a29c6aa5",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/static/js/2.1251a599.chunk.js"
+  },
+  {
+    "revision": "d4a0eefe11f494303c9d",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/static/css/main.f7572a40.chunk.css"
+  },
+  {
+    "revision": "8abec252895e34204516a6773aaaa560",
+    "url": "//cdn.jsdelivr.net/gh/kjy9310/division2@gh-pages/index.html"
+  }
+];
